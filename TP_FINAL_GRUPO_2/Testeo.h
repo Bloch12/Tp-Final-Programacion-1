a7ea69634d625fd1dea346a6311a90afce026b75
Nahuel Bianca <89315607+Bloch12@users.noreply.github.com>
2022-07-18 01:17:28 -0300

    Add files via upload

---
+++ TP_FINAL_GRUPO_2/Testeo.h
@@ -0,0 +1,11 @@
+#ifndef TESTEO_H_INCLUDED
+#define TESTEO_H_INCLUDED
+
+void verArchClientes();
+void verArchConsumos();
+void cargaXConsumos(int carga);
+stConsumos creaConsumoRand(int anio,int id, int validos);
+void altaClientes();
+
+
+#endif // TESTEO_H_INCLUDED
